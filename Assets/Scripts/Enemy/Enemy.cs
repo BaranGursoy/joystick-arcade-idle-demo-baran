04e9e1724548dba69be4420bfe1ae470e3945e5e
BaranGursoy <73434280+BaranGursoy@users.noreply.github.com>
2024-06-05 14:57:14 +0300

    Convert blood splash to particles

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using DG.Tweening;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private GameObject bloodPrefab;
-
+    [SerializeField] private BloodParticleSystem _bloodParticleSystem;
     private int _health = 5;
 
     private void GetHit()
@@ -19,18 +13,14 @@ public class Enemy : MonoBehaviour
         if (_health <= 0)
         {
             GameActions.EnemyDied?.Invoke();
+            _bloodParticleSystem.transform.SetParent(null);
             Destroy(gameObject);
         }
     }
 
     private void SplashBlood()
     {
-        GameObject spawnedBlood = Instantiate(bloodPrefab, transform.position, Quaternion.identity);
-
-        Vector3 bloodSplashPos = transform.position + (Random.onUnitSphere * Random.Range(3f, 5f));
-        bloodSplashPos.y = 0.1f;
-
-        spawnedBlood.transform.DOJump(bloodSplashPos, 3f, 1, 0.3f);
+        _bloodParticleSystem.Play();
     }
 
     private void OnTriggerEnter(Collider other)
---
+++ Assets/Scripts/World/BloodParticleSystem.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class BloodParticleSystem : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem bloodParticleSystem;
+    [SerializeField] private GameObject bloodSplatPrefab;
+    private List<ParticleCollisionEvent> _collisionEvents = new();
+
+
+    public void Play()
+    {
+        bloodParticleSystem.Play();
+    }
+    
+    private void OnParticleCollision(GameObject other)
+    {
+        if (!other.gameObject.CompareTag("Floor")) return;
+        if (!bloodParticleSystem) return;
+            
+        int numCollisionEvents = bloodParticleSystem.GetCollisionEvents(other, _collisionEvents);
+        
+        int i = 0;
+        while (i < numCollisionEvents)
+        {
+            Vector3 pos = _collisionEvents[i].intersection;
+            SpawnBloodSplat(pos);
+            i++;
+        }
+    }
+    
+    private void SpawnBloodSplat(Vector3 contactPoint)
+    {
+        contactPoint.y = 0.1f;
+        Instantiate(bloodSplatPrefab, contactPoint, bloodSplatPrefab.transform.rotation);
+    }
+}
