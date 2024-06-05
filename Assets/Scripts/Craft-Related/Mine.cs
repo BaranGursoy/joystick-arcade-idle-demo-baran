ac7aaeadd51554c8a1af5ea7ec2d2deea7accf97
BaranGursoy <73434280+BaranGursoy@users.noreply.github.com>
2024-06-06 01:22:46 +0300

    Implement mine scaling mechanic

--- Assets/Scripts/Craft-Related/Mine.cs
+++ Assets/Scripts/Craft-Related/Mine.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Mine : Interactable
 {
     [SerializeField] private MineRespawner mineRespawner;
+    [SerializeField] private Transform mineParentTransform;
 
     [SerializeField] private int mineHealth = 7;
     [SerializeField] private float mineRespawnTime = 3f;
@@ -12,9 +13,12 @@ public class Mine : Interactable
     private int _mineStartingHealth;
     private bool _isMineFinished = false;
 
+    private float _scaleDecreaseFactor;
+
     private void Start()
     {
         _mineStartingHealth = mineHealth;
+        _scaleDecreaseFactor = 1f / mineHealth;
     }
 
     private void Update()
@@ -25,27 +29,39 @@ public class Mine : Interactable
 
         if (passedTimeBetweenCollectibleSpawns >= collectibleSpawnRate)
         {
-            Collectible spawnedCollectible = SpawnCollectible(transform);
-
-            DecreaseMineHealth();
-            
-            GameActions.PlaySfxAction?.Invoke(SFXType.Mine);
-            
-            if(playerController.StackIsEmpty || (playerController.StackHasEmptySpace() && playerController.PeekStack().GetType() == typeof(Ore)))
-            {
-                AddToPlayerStack();
-                SendCollectibleToPlayerStack();
-            }
-
-            else if (spawnedCollectible is Ore spawnedOre)
-            {
-                SendOreToRandomPlace(spawnedOre);
-            }
-            
-            ResetPassedTime();
+            SpawnOreFromMine();
         }
         
         passedTimeBetweenCollectibleSpawns += Time.deltaTime;
+    }
+
+    private void SpawnOreFromMine()
+    {
+        Collectible spawnedCollectible = SpawnCollectible(transform);
+
+        AdjustScaleOfMine();
+
+        DecreaseMineHealth();
+            
+        GameActions.PlaySfxAction?.Invoke(SFXType.Mine);
+            
+        if(playerController.StackIsEmpty || (playerController.StackHasEmptySpace() && playerController.PeekStack().GetType() == typeof(Ore)))
+        {
+            AddToPlayerStack();
+            SendCollectibleToPlayerStack();
+        }
+
+        else if (spawnedCollectible is Ore spawnedOre)
+        {
+            SendOreToRandomPlace(spawnedOre);
+        }
+            
+        ResetPassedTime();
+    }
+
+    private void AdjustScaleOfMine()
+    {
+        mineParentTransform.localScale -= (Vector3.one * _scaleDecreaseFactor);
     }
 
     private void DecreaseMineHealth()
@@ -108,6 +124,8 @@ public class Mine : Interactable
         {
             playerController.ActivateAndSwingPickaxe(collectibleSpawnRate);
         }
+        
+        mineParentTransform.localScale = Vector3.one;
         
         mineHealth = _mineStartingHealth;
         _isMineFinished = false;
