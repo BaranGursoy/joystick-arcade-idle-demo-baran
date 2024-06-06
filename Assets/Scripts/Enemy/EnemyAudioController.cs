b9ff68a86e1a0cb651003e0f4452edf46919cd83
BaranGursoy <73434280+BaranGursoy@users.noreply.github.com>
2024-06-06 04:29:39 +0300

    Fix enemy audio controller mutes immediately when enemy dies issue

--- Assets/Scripts/Enemy/EnemyAudioController.cs
+++ Assets/Scripts/Enemy/EnemyAudioController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyAudioController : MonoBehaviour
@@ -7,15 +8,29 @@ public class EnemyAudioController : MonoBehaviour
     private void OnEnable()
     {
         GameActions.PlayEnemyHitSfx += PlayEnemyHitSFX;
+        GameActions.EnemyDied += AdjustEnemyAudioControllerAfterEnemyDies;
     }
 
     private void OnDestroy()
     {
         GameActions.PlayEnemyHitSfx -= PlayEnemyHitSFX;
+        GameActions.EnemyDied -= AdjustEnemyAudioControllerAfterEnemyDies;
     }
 
     private void PlayEnemyHitSFX()
     {
         enemyAudioSource.Play();
+    }
+
+    private void AdjustEnemyAudioControllerAfterEnemyDies()
+    {
+        transform.SetParent(null);
+        StartCoroutine(DeleteEnemyAudioControllerCoroutine());
+    }
+
+    private IEnumerator DeleteEnemyAudioControllerCoroutine()
+    {
+        yield return new WaitUntil(() => !enemyAudioSource.isPlaying);
+        Destroy(gameObject);
     }
 }
